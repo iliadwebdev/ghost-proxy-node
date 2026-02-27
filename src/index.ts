15122a2feb4e25cf654a07a735a263b5714d0426
Owen Rossi-Keen <owenrossikeen@gmail.com>
2026-02-27 16:07:50 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -20,11 +20,14 @@ const targets: Record<string, string> = {
 
 await checkTargets(targets);
 
+// ORDER IS IMPORTANT — specific routes first, Next.js catch-all last
 const server = createServer([
   activityPubRoute(config.ACTIVITYPUB_PROXY_TARGET),
   analyticsRoute(config.ANALYTICS_PROXY_TARGET),
-  nextjsRoute(config.NEXTJS_INTERNAL_URL),
   ghostRoute(config.GHOST_INTERNAL_URL),
+
+  // Next should probably come last no matter what
+  nextjsRoute(config.NEXTJS_INTERNAL_URL),
 ]);
 
 server.listen(config.PORT, () => {
