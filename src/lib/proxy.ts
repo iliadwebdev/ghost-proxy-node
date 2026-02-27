5e4f17e0e65efe31d18c20d8c9751b103608a639
Owen Rossi-Keen <owenrossikeen@gmail.com>
2026-02-27 16:11:47 -0500

    Update proxy.ts

--- src/lib/proxy.ts
+++ src/lib/proxy.ts
@@ -2,8 +2,8 @@ import { logResponse, logError } from "./logger.js";
 import httpProxy from "http-proxy";
 
 export const proxy = httpProxy.createProxyServer({
-  secure: false,
-  changeOrigin: true,
+  changeOrigin: true, // The origin should be changed to the target URL.
+  secure: false, // Because we are proxying to internal services via HTTP, we don't want to reject self-signed certs. Is this a security risk?
 });
 
 proxy.on("proxyRes", (proxyRes, req) => {
